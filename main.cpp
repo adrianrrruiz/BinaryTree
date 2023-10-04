5ae3fbd75f369d6328988203f7ea416bff1965cd
Adrian Ruiz <adrianruiz0208@gmail.com>
2023-10-04 11:30:31 -0500

    Interfaz para probar arbol AVL

--- main.cpp
+++ main.cpp
@@ -1,93 +1,86 @@
 #include <iostream>
 #include "binary_tree/BinaryTree.hpp"
-#include "person/Person.hpp"
 
-int main(int argc, char const *argv[])
+int main()
 {
+    int opcion, dato;
+    BinaryTree<int> arbol = BinaryTree<int>();
 
-    BinaryTree<int> myTree = BinaryTree<int>(5);
-
-    cout << myTree.getRoot() << endl;
-
-    /*
-    myTree.addNode(3);
-    myTree.addNode(7);
-    myTree.addNode(2);
-    myTree.addNode(4);
-    myTree.addNode(6);
-    myTree.addNode(8);
-    myTree.addNode(9);
-    myTree.addNode(10);
-    myTree.addNode(0);
-    myTree.addNode(1);
-*/
-    /*
-    //prueba right-left
-    myTree.addNode(10);
-    myTree.addNode(5);
-    myTree.addNode(20);
-    myTree.addNode(15);
-    myTree.addNode(21);
-    myTree.addNode(14);
-*/
-    //Prueba left-right
-    myTree.addNode(10);
-    myTree.addNode(6);
-    myTree.addNode(20);
-    myTree.addNode(4);
-    myTree.addNode(8);
-    myTree.addNode(7);
-
-
-    list<int> myList = myTree.preOrder();
-
-    for (auto it = myList.begin(); it != myList.end(); it++)
-    {
-        cout << *it << " ";
-    }
-    //prueba eliminar
-    myTree.deleteNode(10);
-
-    myList = myTree.preOrder();
-
-    for (auto it = myList.begin(); it != myList.end(); it++)
-    {
-        cout << *it << " ";
-    }
-
-    //fin prueba eliminar
-
-    BinaryTree<Person> myPersonsTree = BinaryTree<Person>(Person("Juan", "Perez", 20));
-
-    cout << endl
-         << myPersonsTree.getRoot() << endl;
-
-    myPersonsTree.addNode(Person("Pedro", "Gomez", 30));
-    myPersonsTree.addNode(Person("Karol", "Santana", 5));
-    myPersonsTree.addNode(Person("Maria", "Lopez", 40));
-    myPersonsTree.addNode(Person("Jose", "Garcia", 50));
-    myPersonsTree.addNode(Person("Ana", "Martinez", 60));
-    myPersonsTree.addNode(Person("Luis", "Sanchez", 70));
-    myPersonsTree.addNode(Person("Laura", "Rodriguez", 80));
-
-    list<Person> myPersonsList = myPersonsTree.preOrder();
-
-    for (auto it = myPersonsList.begin(); it != myPersonsList.end(); it++)
-    {
-        cout << *it << " ";
-    }
-    BinaryNode<int>* nodoAux;
-    nodoAux = myTree.getRootNode();
-    int altura = myTree.alturaNodo(nodoAux);
-
-    cout<<"\n"<<altura<<endl;
-
-    myList = myTree.preOrder();
-
-    for (auto it = myList.begin(); it != myList.end(); it++)
-    {
-        cout << *it << " ";
-    }
+    do{
+        cout << "ARBOL BINARIO AVL\n";
+        cout << "1. Ingresar un dato\n";
+        cout << "2. Mostrar arbol con preorden\n";
+        cout << "3. Mostrar arbol con inorden\n";
+        cout << "4. Mostrar arbol con posorden\n";
+        cout << "5. Eliminar un dato\n";
+        cout << "6. Altura del arbol\n";
+        cout << "7. Salir\n";
+        cout << "Ingrese una opcion: "; cin >> opcion;
+        
+        switch (opcion)
+        {
+            case 1:
+                cout << "Ingrese el numero que quiera insertar al arbol: ";
+                cin >> dato;
+                arbol.addNode(dato);
+                system("pause");
+                break;
+            case 2:{
+                cout << "PREORDEN\n";
+                list<int> myList = arbol.preOrder();
+                for (auto it = myList.begin(); it != myList.end(); it++)
+                {
+                    cout << *it << " ";
+                }
+                cout << endl;
+                system("pause");
+                break;
+            }
+            case 3:{
+                cout << "INORDEN\n";
+                list<int> myList = arbol.inOrder();
+                for (auto it = myList.begin(); it != myList.end(); it++)
+                {
+                    cout << *it << " ";
+                }
+                cout << endl;
+                system("pause");
+                break;
+            }
+            case 4:{
+                cout << "POSORDEN\n";
+                list<int> myList = arbol.postOrder();
+                for (auto it = myList.begin(); it != myList.end(); it++)
+                {
+                    cout << *it << " ";
+                }
+                cout << endl;
+                system("pause");
+                break;
+            }
+            case 5: 
+                cout << "Ingrese el numero que quiera eliminar al arbol: ";
+                cin >> dato;
+                arbol.deleteNode(dato);
+                break;
+            case 6:{
+                BinaryNode<int>* nodoAux = arbol.getRootNode(); //Se le pide la raiz del arbol
+                int altura = arbol.alturaNodo(nodoAux); //Se pasa por parametro para saber la altura
+                cout << "La altura del arbol es: " << altura << endl;
+                system("pause");
+                break;
+            }
+            case 7:
+                cout << "Muchas gracias por usar el sistema :)\n";
+                system("pause");
+                break;
+            default:
+                cout << "Opcion invalida. Intente de nuevo\n";
+                system("pause");
+                break;
+        }
+        system("cls");
+    }while(opcion != 7);
 
     return 0;
 }
